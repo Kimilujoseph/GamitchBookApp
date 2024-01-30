054d0d4cc86cb80c20c19d95c563efddd087a468
timothy@joseph <timothyjoseph8580@gmail.com>
2024-01-30 23:06:02 +0300

    changed the database connection

--- model/database.js
+++ model/database.js
@@ -2,9 +2,6 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 require("../model/Author")
 require("../model/Book")
-
-const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
