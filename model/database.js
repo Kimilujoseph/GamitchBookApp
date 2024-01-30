7b3f6454c75868e858960e550f4e0f2069ae842b
timothy@joseph <timothyjoseph8580@gmail.com>
2024-01-30 23:58:29 +0300

    connected the db

--- model/database.js
+++ model/database.js
@@ -2,13 +2,17 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 require("../model/Author")
 require("../model/Book")
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-   console.log('Connected to MongoDB');
-});
 
 
+const connect = async function () {
+   try {
+      mongoose.set("strictQuery", true);
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Connected to MongoDB Atlas");
+   } catch (error) {
+      console.log("You're offline");
+      process.exit(1);
+   }
+}
+
+connect()
